443e138f6a3b749189626089df7840573e299b6d
Gabriel Casarini <gariel.335@outlook.com>
2026-03-22 22:57:18 -0300

    Novo mapa e Paralaxe Vertical

--- jogo-plataforma/scripts/player.gd
+++ jogo-plataforma/scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 200
-const JUMP_VELOCITY = -200
+const SPEED = 300
+const JUMP_VELOCITY = -350
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -10,7 +10,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
